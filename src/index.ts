cf83918252bd1dfdd28709c6f7ba560d17514ba5
Alex Littleton <littale12@gmail.com>
2022-02-17 15:29:45 -0500

    WIP - refactor question calls into async functions

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,72 @@
-import { parseBirthdayCsv } from './parsers'
-import * as readline from 'readline'
-import { BirthdayColumnsEnum } from './types'
-import { filterByBirthdayColumn } from './filters'
-import { validateBirthdayHeader } from './validators'
+import { parseBirthdayCsv } from './parsers';
+import * as readline from 'readline';
+import { BirthdayColumnsEnum, BirthdayRow } from './types';
+import { filterByBirthdayColumn } from './filters';
+import { validateBirthdayHeader } from './validators';
+const util = require('util');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-})
+});
 
-rl.question(
-  'Which .csv file do you want to filter? \n',
-  filePath => {
-    const extension = filePath.slice(filePath.length - 4)
+const question = util.promisify(rl.question).bind(rl);
 
-    const table = parseBirthdayCsv(filePath)
-    let columnSelectionQuestion = `What column do you want to filter by? \n`
-    const birthdayColumns = Object.values(BirthdayColumnsEnum).filter(value => typeof value === 'string')
-    birthdayColumns.forEach((column, i)=> {
-      columnSelectionQuestion += `[${i}] ${column}\n`
-    })  
-    rl.question(
-      columnSelectionQuestion,
-      selectedColumn => {
-        const selectedColumnIndex = parseInt(selectedColumn);
-        validateBirthdayHeader(table[0]); // If called earlier, table is an empty array
-        
-        if (!birthdayColumns[selectedColumnIndex]) {
-          console.log(`Invalid selection '${selectedColumn}'. Expected a number between 0 and ${birthdayColumns.length-1}`)
-          rl.close() 
-        } 
+async function firstQuestion() {
+    const filePathQuestion = 'Which .csv file do you want to filter? \n';
+    const filePath = await question(filePathQuestion);
+    const extension = filePath.slice(filePath.length - 4);
 
-        rl.question(`What value do you want to filter for? \n`, filterValue => {
-          const filteredTable = filterByBirthdayColumn(table, selectedColumnIndex, filterValue)
-        
-          if (!filteredTable.length) {
-            console.log(`No matches for '${filterValue}'`)
-            rl.close()
-          }
+    const table = parseBirthdayCsv(filePath);
+    validateBirthdayHeader(table[0]);
+    return table;
+}
 
-          console.log(filteredTable)
-          rl.close()
-          
-        })
+async function secondQuestion() {
+    let columnSelectionQuestion = `What column do you want to filter by? \n`;
+    const birthdayColumns = Object.values(BirthdayColumnsEnum).filter(
+        (value) => typeof value === 'string',
+    );
+    birthdayColumns.forEach((column, i) => {
+        columnSelectionQuestion += `[${i}] ${column}\n`;
+    });
+    const selectedColumn = await question(columnSelectionQuestion);
+    const selectedColumnIndex = parseInt(selectedColumn);
 
-    })
-})
+    if (!birthdayColumns[selectedColumnIndex]) {
+        console.log(
+            `Invalid selection '${selectedColumn}'. Expected a number between 0 and ${
+                birthdayColumns.length - 1
+            }`,
+        );
+        rl.close();
+    }
 
-rl.on('close', () => {
-    process.exit(0)
-})
+    return selectedColumnIndex;
+}
+
+async function thirdQuestion() {
+    const filterValue = await question(
+        `What value do you want to filter for? \n`,
+    );
+    return filterValue;
+}
+
+rl.on('close', async () => {
+    process.exit(0);
+});
+
+const filteredTable = async () => {
+    await filterByBirthdayColumn(
+        await firstQuestion(),
+        await secondQuestion(),
+        await thirdQuestion(),
+    );
+};
+
+if (!filteredTable.length) {
+    rl.close();
+}
+
+console.log(filteredTable);
+rl.close();
